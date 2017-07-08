36c9c2e6f6e65e66196ceac3809316cd01c68ea7
Sergey Borodanov <s.borodanov@gmail.com>
2017-07-09 01:18:38 +0700

    first tryings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+enum Cell {
+    Empty,
+    Wall,
+    Player
+}
+
+fn get_symbol( cell: &Cell ) -> char {
+    match cell {
+        Empty => '_',
+        Wall =>  '#',
+        Player => '@'
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut game_field = [ [ Cell::Empty, Cell::Wall ],
+                           [ Cell::Empty, Cell::Empty ] ];
+
+    // let tmp_char = get_symbol( &game_field[0][0] );
+
+    for row in game_field.into_iter() {
+        for cell in row.into_iter() {
+            let tmp = get_symbol( &cell );
+            println!("{}", tmp );
+        }
+
+        println!( "\r\n" );
+    }
 }
