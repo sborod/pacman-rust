b76ee41ba152923323512fb5d70b8ad8c1089c0e
Vladimir Golovchansky <golovchansky.vlad@gmail.com>
2017-07-09 15:46:48 +0700

    Add dynamic array (Vec); fix printing different symbols

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,46 @@
 enum Cell {
     Empty,
     Wall,
-    Player
+    Player,
 }
 
-fn get_symbol( cell: &Cell ) -> char {
+fn get_symbol( cell: Cell ) -> char {
     match cell {
-        Empty => '_',
-        Wall =>  '#',
-        Player => '@'
+        Cell::Empty => '_',
+        Cell::Wall =>  '#',
+        Cell::Player => '@'
     }
 }
 
+fn initialize_field( ) -> Vec<Vec<Cell>>{
+    //ToDo: parse from file
+
+    let mut result_field: Vec<Vec<Cell>> = Vec::new();
+
+    let mut row: Vec<Cell> = Vec::new();
+    row.push(Cell::Empty);
+    row.push(Cell::Wall);
+    result_field.push(row);
+
+    let mut row: Vec<Cell> = Vec::new();
+    row.push(Cell::Player);
+    row.push(Cell::Wall);
+    result_field.push(row);
+
+    return result_field;
+}
+
 fn main() {
-    let mut game_field = [ [ Cell::Empty, Cell::Wall ],
-                           [ Cell::Empty, Cell::Empty ] ];
+    let game_field = initialize_field();
 
     // let tmp_char = get_symbol( &game_field[0][0] );
 
     for row in game_field.into_iter() {
         for cell in row.into_iter() {
-            let tmp = get_symbol( &cell );
-            println!("{}", tmp );
+            let tmp = get_symbol( cell );
+            print!("{}", tmp );
         }
 
-        println!( "\r\n" );
+        println!( "" );
     }
 }
